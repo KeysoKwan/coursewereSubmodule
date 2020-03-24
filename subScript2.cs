d60bb2614a2a60928fb36fe9f89ff261a9ad5953
keyso <keyso000@outlook.com>
2020-03-24 16:11:26 +0800

    修改了预制物,新增了脚本

---
+++ subScript2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subScript2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("subScript2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, 50.0f * Time.deltaTime, 0.0f));
+    }
+}
